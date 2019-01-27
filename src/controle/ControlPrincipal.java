6025973ff408f649a6a341c1a89136b1e4e1f396
COOPERATIVA <COOPERATIVA@COOPERATIVA-PC>
2019-01-27 11:36:39 -0300

    Alteração menu principal > adicionado novos menus e combinação de ctrl + U como atalho para cadastrar usuários novos

--- src/controle/ControlPrincipal.java
+++ src/controle/ControlPrincipal.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 /**
@@ -27,10 +28,16 @@ public class ControlPrincipal implements Initializable {
     @FXML
     MenuItem fechar;
 
+    @FXML
+    MenuItem idCadUser;
+
     @Override
     public void initialize(java.net.URL location,
             java.util.ResourceBundle resources) {
         ola.setText("ola");
+        idCadUser.setAccelerator(
+                KeyCombination.keyCombination("SHORTCUT+U")
+        );
     }
 
     @FXML
@@ -38,15 +45,16 @@ public class ControlPrincipal implements Initializable {
         try {
             new PUsuario().start(new Stage());
         } catch (Exception e) {
-            System.out.println(""+e);
+            System.out.println("" + e);
         }
     }
+
     @FXML
     void viewUsuario(ActionEvent event) {
         try {
             new TabelaUser().start(new Stage());
         } catch (Exception e) {
-            System.out.println(""+e);
+            System.out.println("" + e);
         }
     }
 
