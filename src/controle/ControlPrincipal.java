870f535479e51ae812c33af8b2ff1ac27677f08a
rafaelstavares <rafael_tavares@msn.com>
2019-12-28 14:57:04 -0300

    teste na tela principal

--- src/controle/ControlPrincipal.java
+++ src/controle/ControlPrincipal.java
@@ -7,14 +7,21 @@ package controle;
 
 import coopertaxi.PUsuario;
 import coopertaxi.TabelaUser;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
@@ -22,29 +29,51 @@ import javafx.stage.Stage;
  */
 public class ControlPrincipal implements Initializable {
 
-
-    @FXML
+/*    @FXML
     MenuItem fechar;
 
     @FXML
     MenuItem idCadUser;
+*/
+    @FXML
+    Label lHora;
 
     @Override
     public void initialize(java.net.URL location,
             java.util.ResourceBundle resources) {
-        idCadUser.setAccelerator(
+        /*idCadUser.setAccelerator(
                 KeyCombination.keyCombination("SHORTCUT+U")
-        );
+        );*/
+        bindToTime();
+ 
     }
 
     @FXML
     void cadusuario(ActionEvent event) {
         try {
             new PUsuario().start(new Stage());
-            
+
         } catch (Exception e) {
             System.out.println("" + e);
         }
+    }
+
+    private void bindToTime() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0),
+                        new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        Calendar time = Calendar.getInstance();
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+                        lHora.setText(simpleDateFormat.format(time.getTime()));
+                    }
+                }
+                ),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
     @FXML
